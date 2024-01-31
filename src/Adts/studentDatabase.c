76a2e31e161273381de9f3ed92290bf7715b18c4
Chris <cl5522@ic.ac.uk>
2024-01-31 17:34:24 +0000

    Cleaning ADTS lesson, and completing the fileHandling.c file

--- src/Adts/studentDatabase.c
+++ src/Adts/studentDatabase.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-#include "dynArray.h"
+#include "dynarray.h"
 
 #define MAX_NAME_LENGTH 50
 
--- src/fileHandling.c
+++ src/fileHandling.c
@@ -9,13 +9,44 @@ typedef struct Data *data;
 
 int main() {
   FILE *file;
-  file = fopen("example.txt", "r");
+  file = fopen("src/example.txt", "r");
 
   if (file == NULL) {
     perror("Error opening the file");
   }
 
-  data d = (d)malloc(sizeof(struct data));
+  data d = (data) malloc(sizeof(struct Data));
 
+  if (d == NULL) {
+    perror("Error allocating memory");
+    fclose(file);
+    return 1;
+  }
 
+  // finding the size of the file
+  fseek(file, 0, SEEK_END);
+  long fileSize = ftell(file);
+  rewind(file);
+
+  // creating enough space for the file size
+  d->content = (char *)malloc(fileSize + 1); // +1 for null terminator
+
+  if (d->content == NULL) {
+      perror("Error allocating memory");
+      free(d);
+      fclose(file);
+      return 1; // Return an error code
+  }
+
+  // read the content of the file into the struct
+  fread(d->content, 1, fileSize, file);
+  d->content[fileSize] = '\0'; // Null-terminate the string
+
+  printf("File Content:\n%s\n", d->content);
+
+  free(d->content);
+  free(d);
+  fclose(file);
+
+  return 0;
 }
